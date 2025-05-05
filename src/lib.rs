f23ba217f897add6c44ede4ef0e849ad50d79b55
Christina Chortaria <actspatial@gmail.com>
2025-05-05 16:20:05 -0400

    Use config.yml to read the FIGGY_URL var

--- src/lib.rs
+++ src/lib.rs
@@ -23,8 +23,9 @@ pub struct CatalogClient {
 
 impl CatalogClient {
     pub fn new() -> Self {
+        let figgy_url = std::env::var("FIGGY_URL");
         CatalogClient {
-            url: "https://figgy-staging.princeton.edu/catalog.json?f%5Bephemera_project_ssim%5D%5B%5D=Born+Digital+Monographs%2C+Serials%2C+%26+Series+Reports&f%5Bhuman_readable_type_ssim%5D%5B%5D=Ephemera+Folder&f%5Bstate_ssim%5D%5B%5D=complete&per_page=100&q=".to_string(),
+            url: figgy_url.unwrap()
         }
     }
 
@@ -41,6 +42,7 @@ mod tests {
 
     #[tokio::test]
     async fn test_get_data() {
+        std::env::set_var("FIGGY_URL", "https://figgy-staging.princeton.edu/catalog.json?f%5Bephemera_project_ssim%5D%5B%5D=Born+Digital+Monographs%2C+Serials%26Series+Reports&f%5Bhuman_readable_type_ssim%5D%5B%5D=Ephemera+Folder&f%5Bstate_ssim%5D%5B%5D=complete&per_page=100&q=");
         let client = CatalogClient::new();
         let result = client.get_data().await;
         assert!(result.is_ok());
