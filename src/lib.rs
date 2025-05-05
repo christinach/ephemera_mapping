999cadda0dc16bea8582b9982b52fa619bfb0f29
Christina Chortaria <actspatial@gmail.com>
2025-05-05 18:47:25 -0400

    use Default for CatalogClient per `cargo clippy` suggestion

--- src/lib.rs
+++ src/lib.rs
@@ -28,6 +28,12 @@ pub struct CatalogClient {
     pub url: String,
 }
 
+impl Default for CatalogClient {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
 impl CatalogClient {
     pub fn new() -> Self {
         let figgy_url = std::env::var("FIGGY_URL");
@@ -50,7 +56,7 @@ mod tests {
     #[tokio::test]
     async fn test_get_data() {
         std::env::set_var("FIGGY_URL", "https://figgy-staging.princeton.edu/catalog.json?f%5Bephemera_project_ssim%5D%5B%5D=Born+Digital+Monographs%2C+Serials%26Series+Reports&f%5Bhuman_readable_type_ssim%5D%5B%5D=Ephemera+Folder&f%5Bstate_ssim%5D%5B%5D=complete&per_page=100&q=");
-        let client = CatalogClient::new();
+        let client = CatalogClient::default();
         let result = client.get_data().await;
         assert!(result.is_ok());
     }
